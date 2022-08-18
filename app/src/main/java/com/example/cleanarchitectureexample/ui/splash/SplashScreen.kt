d827fc925d5c81e68e91b25bc6dc0a6c836787dd
pawelostafin <pawelostafindev@gmail.com>
2022-08-18 18:02:22 +0200

    splash screen colors updated

--- app/src/main/java/com/example/cleanarchitectureexample/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/example/cleanarchitectureexample/ui/splash/SplashScreen.kt
@@ -1,15 +1,25 @@
 package com.example.cleanarchitectureexample.ui.splash
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.cleanarchitectureexample.ui.theme.AppTheme
 
 @Composable
 fun SplashScreen(viewModel: SplashViewModel) {
 
-    Box(contentAlignment = Alignment.Center) {
-        CircularProgressIndicator()
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.background(
+            color = AppTheme.colors.backgroundPrimary
+        )
+    ) {
+        CircularProgressIndicator(
+            color = AppTheme.colors.primary
+        )
     }
 
 }
