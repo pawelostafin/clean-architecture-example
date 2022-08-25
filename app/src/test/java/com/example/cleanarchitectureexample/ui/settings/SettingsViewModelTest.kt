a9f4dbd78021f163a5546cfff38307bd1a7192ff
pawelostafin <pawelostafindev@gmail.com>
2022-08-25 23:20:13 +0200

    SettingsViewModelTest.kt updated

--- app/src/test/java/com/example/cleanarchitectureexample/ui/settings/SettingsViewModelTest.kt
+++ app/src/test/java/com/example/cleanarchitectureexample/ui/settings/SettingsViewModelTest.kt
@@ -1,7 +1,15 @@
 package com.example.cleanarchitectureexample.ui.settings
 
 import app.cash.turbine.test
+import com.example.domain.model.DarkThemeMode
 import com.example.domain.provider.DispatchersProvider
+import com.example.domain.usecase.ObserveDarkThemeModeUseCase
+import com.example.domain.usecase.SetDarkThemeModeUseCase
+import io.mockk.coEvery
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -24,85 +32,133 @@ import org.koin.test.KoinTestRule
 @OptIn(ExperimentalCoroutinesApi::class)
 class SettingsViewModelTest : BaseTest() {
 
-//    private var _SUT: SettingsViewModel? = null
-//    private val SUT: SettingsViewModel
-//        get() = _SUT!!
-//
-//    private lateinit var observeDarkThemeModeUseCaseMock: ObserveDarkThemeModeUseCase
-//    private lateinit var setDarkThemeModeUseCaseMock: SetDarkThemeModeUseCase
-//
-//    override fun setUp() {
-//        super.setUp()
-//
-//        _SUT = null
-//        observeDarkThemeModeUseCaseMock = mockk()
-//        setDarkThemeModeUseCaseMock = mockk()
-//
-//        setThemeMode(DarkThemeMode.AUTO)
-//    }
-//
-//    override fun tearDown() {
-//        super.tearDown()
-//
-//        verify(atLeast = 1) { observeDarkThemeModeUseCaseMock.execute() }
-//        confirmVerified(observeDarkThemeModeUseCaseMock)
-//        confirmVerified(setDarkThemeModeUseCaseMock)
-//    }
-//
-//    @Test
-//    fun `When backButtonClicked() method is invoked, Back navigation is emitted`() = runTest {
-//        createAndInitializeSUT()
-//
-//        SUT.navigation.test {
-//            SUT.backButtonClicked()
-//            val item = awaitItem()
-//            assertEquals(SettingsViewModel.Navigation.Back, item)
-//        }
-//    }
-//
-//    @Test
-//    fun `StateFlow's Initial values are correct`() = runTest {
-//        val initialDarkThemeMode = DarkThemeMode.AUTO
-//        setThemeMode(initialDarkThemeMode)
-//
-//        createAndInitializeSUT()
-//
-//        verify(exactly = 1) { observeDarkThemeModeUseCaseMock.execute() }
-//        assertEquals(initialDarkThemeMode, SUT.darkThemeMode.value)
-//        assertEquals(false, SUT.darkThemeModeDropdownVisibility.value)
-//    }
-//
-//    @Test
-//    fun `When darkThemeItemClicked() is invoked, darkThemeModeDropdownVisibility changes to true`() = runTest {
-//        createAndInitializeSUT()
-//
-//        SUT.darkThemeModeDropdownVisibility.test {
-//            assertEquals(false, awaitItem())
-//            SUT.darkThemeItemClicked()
-//            expectNoEvents()
-//        }
-//    }
-//
-//    private fun setThemeMode(darkThemeMode: DarkThemeMode) {
-//        every { observeDarkThemeModeUseCaseMock.execute() } returns MutableStateFlow(darkThemeMode)
-//    }
-//
-//    private fun createAndInitializeSUT() {
-//        _SUT = SettingsViewModel(
-//            observeDarkThemeModeUseCase = observeDarkThemeModeUseCaseMock,
-//            setDarkThemeModeUseCase = setDarkThemeModeUseCaseMock
-//        )
-//    }
+    private var _SUT: SettingsViewModel? = null
+    private val SUT: SettingsViewModel
+        get() = _SUT!!
 
+    private lateinit var observeDarkThemeModeUseCaseMock: ObserveDarkThemeModeUseCase
+    private lateinit var setDarkThemeModeUseCaseMock: SetDarkThemeModeUseCase
+
+    override fun setUp() {
+        super.setUp()
+
+        _SUT = null
+        observeDarkThemeModeUseCaseMock = mockk()
+        setDarkThemeModeUseCaseMock = mockk()
+
+        setThemeMode(DarkThemeMode.AUTO)
+    }
+
+    override fun tearDown() {
+        super.tearDown()
+
+        verify(atLeast = 1) { observeDarkThemeModeUseCaseMock.execute() }
+        confirmVerified(observeDarkThemeModeUseCaseMock)
+        confirmVerified(setDarkThemeModeUseCaseMock)
+    }
 
     @Test
-    fun booleanFlowTest() = runTest {
-        val booleanFlow = MutableStateFlow(false)
-        booleanFlow.test {
+    fun `When backButtonClicked() method is invoked, Back navigation is emitted`() = runTest {
+        createSut()
+
+        SUT.navigation.test {
+            SUT.backButtonClicked()
+            val item = awaitItem()
+            assertEquals(SettingsViewModel.Navigation.Back, item)
+        }
+    }
+
+    @Test
+    fun `StateFlow's Initial values are correct`() = runTest {
+        val initialDarkThemeMode = DarkThemeMode.AUTO
+        setThemeMode(initialDarkThemeMode)
+
+        createSut(initialize = false)
+
+        verify(exactly = 1) { observeDarkThemeModeUseCaseMock.execute() }
+        assertEquals(initialDarkThemeMode, SUT.darkThemeMode.value)
+        assertEquals(false, SUT.darkThemeModeDropdownVisibility.value)
+    }
+
+    @Test
+    fun `When darkThemeItemClicked() is invoked, darkThemeModeDropdownVisibility changes to true`() = runTest {
+        createSut()
+
+        SUT.darkThemeModeDropdownVisibility.test {
             assertEquals(false, awaitItem())
-            booleanFlow.value = true
-//            assertEquals(true, awaitItem())
+            SUT.darkThemeItemClicked()
+            assertEquals(true, awaitItem())
             expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `When darkThemeDropdownDismissRequested() is invoked, darkThemeModeDropdownVisibility changes from true to false`() = runTest {
+        createSut()
+
+        SUT.darkThemeModeDropdownVisibility.test {
+            assertEquals(false, awaitItem())
+            SUT.darkThemeItemClicked()
+            assertEquals(true, awaitItem())
+            SUT.darkThemeDropdownDismissRequested()
+            assertEquals(false, awaitItem())
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `When darkThemeModeChangeRequested() is invoked, setDarkThemeModeUseCase is invoked with correct value and darkThemeModeDropdownVisibility changes from true to false`() =
+        runTest {
+            coEvery { setDarkThemeModeUseCaseMock.execute(any()) } returns Unit
+            createSut()
+
+            val newValue = DarkThemeMode.DISABLED
+
+            SUT.darkThemeModeDropdownVisibility.test {
+                assertEquals(false, awaitItem())
+                SUT.darkThemeItemClicked()
+                assertEquals(true, awaitItem())
+                SUT.darkThemeModeChangeRequested(newValue)
+                assertEquals(false, awaitItem())
+                expectNoEvents()
+            }
+
+            verify(exactly = 1) { setDarkThemeModeUseCaseMock.execute(newValue) }
+        }
+
+    @Test
+    fun `When DarkThemeMode is updated, its emitted to view`() = runTest {
+        val fakeFlow = MutableStateFlow(DarkThemeMode.DISABLED)
+        every { observeDarkThemeModeUseCaseMock.execute() } returns fakeFlow
+
+        createSut()
+
+        SUT.darkThemeMode.test {
+            assertEquals(DarkThemeMode.DISABLED, awaitItem())
+
+            fakeFlow.value = DarkThemeMode.AUTO
+            assertEquals(DarkThemeMode.AUTO, awaitItem())
+
+            fakeFlow.value = DarkThemeMode.ENABLED
+            assertEquals(DarkThemeMode.ENABLED, awaitItem())
+
+            fakeFlow.value = DarkThemeMode.DISABLED
+            assertEquals(DarkThemeMode.DISABLED, awaitItem())
+        }
+        verify(exactly = 2) { observeDarkThemeModeUseCaseMock.execute() }
+    }
+
+    private fun setThemeMode(darkThemeMode: DarkThemeMode) {
+        every { observeDarkThemeModeUseCaseMock.execute() } returns MutableStateFlow(darkThemeMode)
+    }
+
+    private fun createSut(initialize: Boolean = true) {
+        _SUT = SettingsViewModel(
+            observeDarkThemeModeUseCase = observeDarkThemeModeUseCaseMock,
+            setDarkThemeModeUseCase = setDarkThemeModeUseCaseMock
+        )
+        if (initialize) {
+            SUT.initializeIfNeeded()
         }
     }
 
